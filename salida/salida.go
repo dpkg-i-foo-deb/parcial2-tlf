2078fd9b1437b669a4aa07f16e136d96e7b4e23e
StivenHerreraSierra <stiven.herreras@uqvirtual.edu.co>
2022-11-18 12:51:29 -0500

    Formato en la tabla generada

--- salida/salida.go
+++ salida/salida.go
@@ -4,14 +4,16 @@ import (
 	"analizador-lexico/analizador"
 	"analizador-lexico/util"
 	"errors"
+	"fmt"
 	"os"
+	"strings"
 )
 
 var contenido []byte
 
 func EscribirArchivo() {
-
-	contenido = append(contenido, []byte("Tipo de Token"+"\t"+"Lexema\t\n")...)
+	linea := obtenerCabecera()
+	contenido = append(contenido, []byte(linea)...)
 
 	if _, err := os.Stat("salida.txt"); errors.Is(err, os.ErrNotExist) {
 		_, err = os.Create("salida.txt")
@@ -20,11 +22,23 @@ func EscribirArchivo() {
 	}
 
 	for _, t := range analizador.Tokens {
-		contenido = append(contenido, []byte(t.Categoria+"\t"+t.Valor+"\n")...)
+		linea = fmt.Sprintf("| %-40s | %-50s |\n", t.Categoria, t.Valor)
+		contenido = append(contenido, []byte(linea)...)
 	}
 
 	err := os.WriteFile("salida.txt", contenido, 0644)
 
 	util.VerificarError(err)
 
+}
+
+func obtenerCabecera() string {
+	separador := "|" + strings.Repeat("-", 42) + "|" + strings.Repeat("-", 52) + "|\n"
+
+	cabecera := separador
+	cabecera += fmt.Sprintf("| %-40s |", strings.Repeat(" ", 12)+"Tipo de Token")
+	cabecera += fmt.Sprintf(" %-50s |\n", strings.Repeat(" ", 20)+"Lexema")
+	cabecera += separador
+
+	return cabecera
 }
